f179ba92ccf6fe5d79bea71e328f1d9fb8dddeb0
Revilium <132856761+Revilium@users.noreply.github.com>
2023-05-14 21:20:58 +0800

    Antivirus.lua

--- Antivirus.lua
+++ Antivirus.lua
@@ -1,1 +1,11 @@
+-- Script By: DiemzMaster, AlohaGryphin, PorQueTorQue
 
+-- Idea By: 65GamingHolie, VCGDuparier, HolayMolay, ChickenKFCEten
+
+-- Make sure to join our discord!
+
+-- https://discord.com/invite/DJousting
+
+-- (If it's expired it's not my fault you're late)
+
+Https://Idiot.com
